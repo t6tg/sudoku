229b6937ca0b67acc1d1c5e87b9e468ce4dbca00
thanawatgulati <thanawatgulatijames@gmail.com>
2020-02-09 19:33:29 +0700

    💅🏼 pretty code

--- src/Cell.js
+++ src/Cell.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Cell extends Component {
-  render() {
-    return (
-      <div
-        onClick={e => {
-          if (this.props.isInitial) {
-            return;
-          }
-          this.props.onChange((this.props.num + 1) % 5);
-        }}
-        className={`cell ${this.props.isInitial ? 'initial' : ''}`}
-      >
-        {this.props.num !== 0 && this.props.num}
-      </div>
-    );
-  }
-}
+const Cell = ({ isInitial, num, onChange }) => {
+  return (
+    <div
+      onClick={e => {
+        if (isInitial) {
+          return;
+        }
+        onChange((num + 1) % 5);
+      }}
+      className={`cell ${isInitial ? 'initial' : ''}`}
+    >
+      {num !== 0 && num}
+    </div>
+  );
+};
 
 export default Cell;
